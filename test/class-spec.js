246a10925bcafde141216e9737f9d967263b1897
Samuel Park <s.park111497@gmail.com>
2022-08-18 20:36:56 -0700

    finished exercise

--- test/class-spec.js
+++ test/class-spec.js
@@ -4,31 +4,42 @@ const expect = chai.expect;
 const { Word } = require("../class");
 
 describe("Word", function () {
+  let potato;
+  this.beforeEach(() => {
+    potato = new Word("Potato");
+  });
+
   describe("Word constructor function", function () {
     it('should have a "word" property', function () {
-      expect.fail("replace with your code");
+      expect(potato).to.have.property("word");
     });
-  
+
     it('should set the "word" property when a new word is created', function () {
-      expect.fail("replace with your code");
+      expect(potato.word).to.equal("Potato");
     });
   });
 
   describe("removeVowels function", function () {
     it("should return a the word with all vowels removed", function () {
-      expect.fail("replace with your code");
+      potato.removeVowels();
+
+      expect(potato.removeVowels()).to.equal("Ptt");
     });
   });
 
   describe("removeConsonants function", function () {
     it("should return the word with the consonants removed", function () {
-      expect.fail("replace with your code");
+     potato.removeConsonants();
+
+      expect(potato.removeConsonants()).to.equal("oao");
     });
   });
-  
+
   describe("pigLatin function", function () {
     it("should return the word converted to pig latin", function () {
-      expect.fail("replace with your code");
+      potato.pigLatin();
+
+      expect(potato.pigLatin()).to.equal("otatoPay");
     });
   });
 });
